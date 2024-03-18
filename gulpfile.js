7576929c43acef800aa2f790c33e550f3d0f5296
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2024-03-18 17:39:18 -0400

    Convert css to scss; adjust previous styles to allow for two column layout

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ uswds.settings.version = 3;
 
 uswds.paths.dist.theme = './_theme';
 
+uswds.paths.src.projectSass = "./assets/css/"
+
 exports.init = uswds.init;
 exports.compile = uswds.compile;
 exports.watch = uswds.watch;
